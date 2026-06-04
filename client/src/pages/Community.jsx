b4b1663b785fc69d05ea9dfed47b07f7acb550ed
yashkodgirwar <yashkodgirwar@gmail.com>
2026-06-04 18:54:27 +0530

    Build the community page

--- client/src/pages/Community.jsx
+++ client/src/pages/Community.jsx
@@ -1,10 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {useEffect,useState} from 'react'
+import {dummyPublishedCreationData} from '../assets/assets'
 
 const Community = () => {
+  const [creations,setCreations] =useState([])
+  const {user} =useUser()
+
+  const fetchCreation= async ()=>{
+    setCreations(dummyPublishedCreationData)
+  }
+  useEffect(()=>{
+      if(user){
+        fetchCreation()
+      }
+  },[user])
   return (
-    <div>
-      <h1>Community</h1>
+    <div className="bg-white h-full w-full rounded-xl overflow-y-scroll">
+  {creations.map((creation, index) => (
+    <div
+      key={index}
+      className="relative group inline-block pl-3 pt-3 w-full sm:w-1/2 lg:w-1/3"
+    >
+      <img
+        src={creation.content}
+        alt=""
+        className="w-full h-full object-cover rounded-lg"
+      />
+
+      <div
+        className="absolute inset-0 flex gap-2 items-end justify-end 
+                   group-hover:justify-between p-3 
+                   group-hover:bg-gradient-to-b from-transparent to-black/80 
+                   text-white rounded-lg"
+      >
+        <p className="text-sm hidden group-hover:block">{creation.prompt}</p>
+        <div className="flex gap-1 items-center">
+          <p>{creation.likes.length}</p>
+          <Heart
+            className={`min-w-5 h-5 hover:scale-110 cursor-pointer 
+              ${creation.likes.includes(user.id) 
+                ? 'fill-red-500 text-red-600' 
+                : 'text-white'}`}
+          />
+        </div>
+      </div>
     </div>
+  ))}
+</div>
+
   )
 }
 
