06f6e4b415293c8cd2f2e7e8aac3fdc158a92b4a
yashkodgirwar <yashkodgirwar@gmail.com>
2026-06-10 15:50:46 +0530

    made the middleware to check the user has premium plan or not so we can set the free usage limit

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,1 +1,26 @@
+import { clerkClient } from "@clerk/express";
+import { useId } from "react";
+
 //middleware to check the userId and hasPremiumPlan
+export const auth=async(req,res,next)=>{
+    try{
+        const {userId,has}=await req.auth();
+     const hasPremiumPlan = user.publicMetadata?.plan === "premium";
+        const user=await clerkClient.user.getUser(userId);
+        if(!hasPremiumPlan && user.privateMetadata.free_usage){
+            req.free_usage=user.privateMetadata.free_usage
+        }else{
+            await clerkClient.users.updateUserMetadata(userId,{
+                privateMetadata:{
+                    free_usage:0
+                }
+            })
+            req.free_usage=0;
+        }
+        req.plan=hasPremiumPlan ? 'premium' : 'free';
+        next()
+
+    }catch(error){
+        res.json({success:false, message:error.message})
+    }
+}
