6daed6efb776713491a751d708cd0b04724d55b1
yashkodgirwar <yashkodgirwar@gmail.com>
2026-06-10 17:24:27 +0530

    build the ai controller for gererateBlogtitle

--- server/controller/aiController.js
+++ server/controller/aiController.js
@@ -25,7 +25,7 @@ export const generateArticle=async (req,res)=>{
             {
                 role: "user",
                 content:prompt,
-            },
+            }
         ],
         temperature:0.7,
         max_completion_tokens:length,
@@ -36,6 +36,50 @@ export const generateArticle=async (req,res)=>{
     //store the response in data base
     await sql`INSERT INTO creations (user_id,prompt,content,type)
               VALUES (${userId},${prompt},${content}, 'article')`;
+      
+    if(plan !=='premium'){
+        await clerkClient.users.updateUserMetadata(userId,{
+            privateMetadata:{
+                free_usage:free_usage + 1
+            }
+        })
+    }
+    res.json({success:true,content}) 
+
+  }catch(error){
+     console.log(error.message)
+     res.json ({success:false,message:error.message})
+  }
+}
+
+export const generateBlogtitle=async (req,res)=>{
+  try{
+    const {userId}=req.auth();
+    const{prompt,length}=req.body;
+    const plan=req.plan;
+    const free_usage=req.free_usage;
+
+    if(plan !== 'premium' && free_usage >=10){
+        return res.json({success:false, message:"Limit reached. Upgrade to continue ."})
+    }
+
+    const response = await openai.chat.completions.create({
+        model: "gemini-3.5-flash",
+        messages: [
+            {
+                role: "user",
+                content:prompt,
+            }
+        ],
+        temperature:0.7,
+        max_completion_tokens:length,
+    });
+
+    const content =response.choices[0].message.content
+
+    //store the response in data base
+    await sql`INSERT INTO creations (user_id,prompt,content,type)
+              VALUES (${userId},${prompt},${content}, 'blog-title')`;
       
     if(plan !=='premium'){
         await clerkClient.users.updateUserMetadata(userId,{
