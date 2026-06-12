b780f1ac735842927913b9a8f8cb15dbf0712ecd
yashkodgirwar <yashkodgirwar@gmail.com>
2026-06-12 18:30:33 +0530

    connect the dashboard to the backend

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,12 +1,35 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { dummyCreationData } from '../assets/assets'
 import { Gem, Sparkles } from 'lucide-react';
-import { useUser } from "@clerk/react";
+import { useAuth, useUser } from "@clerk/react";
 import Creationitems from '../components/Creationitems';
+import axios from "axios"
+axios.defaults.baseURL=import.meta.env.VITE_BASE_URL;
 
 const Dashboard = () => {
   const { user } = useUser();
-  const creation = dummyCreationData;
+
+
+     const [loading,setLoading] =useState(true);
+  
+       const {gettoken}=useAuth()
+
+       const getDashBaord =async()=>{
+        try{
+    const {data}= await axios.get('/api/user/get-user-creation',{
+      headers:{Authorization : `Bearer ${await getToken()}`}
+    })
+    if(data.success){
+        setContent(data.content)
+      }else{
+        toast.error(data.message)
+      }
+    }catch(error){
+      toast.error(error.message)
+    }
+    setLoading(false);
+  }
+       useEffect(()=>{ getDashBaord()},[])
   return (
     <div className='h-full overflow-y-scroll p-6'>
       <div className='flex justify-start gap-4 flex-wrap'>
@@ -30,14 +53,28 @@ const Dashboard = () => {
         </div>
 
       </div>
+       
+       {
+        loading ? (
+          <div className='flex justify-center items-center h-3/4'>
+<div className='animate-spin rounded-full h-11 w-11 border-3
+border-purple-500 border-t-transparent'></div>
+</div>
 
-      <div className='space-y-3 mt-10'>
+
+          
+        ) :
+        (
+          <div className='space-y-3 mt-10'>
         <p className='mt-6 mb-4'>Recent Creation</p>
         {
           creation.map((item) => <Creationitems key={item.id} item={item} />)
         }
 
       </div>
+        )
+       }
+      
     </div>
   )
 }
