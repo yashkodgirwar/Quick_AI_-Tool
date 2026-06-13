6857c87c7109db0c4abee77f4e854b85a9ba3fc2
yashkodgirwar <yashkodgirwar@gmail.com>
2026-06-13 15:01:39 +0530

    feat(server): register /delete-creation route endpoint

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { auth } from "../middlewares/auth.js";
-import { getPublishedCreation, getUserCreation, togglelikeCration } from "../controller/userController.js";
+import { getPublishedCreation, getUserCreation, togglelikeCration, deleteCreation } from "../controller/userController.js";
 
 const userRouter = express. Router();
 userRouter.get('/get-user-creations', auth, getUserCreation)
 userRouter.get('/get-published-creations', auth, getPublishedCreation)
 userRouter.post('/toggle-like-creation', auth, togglelikeCration)
+userRouter.post('/delete-creation', auth, deleteCreation)
 
 export default userRouter;
 
