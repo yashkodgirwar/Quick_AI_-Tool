148e4ebfd39588db513613c7d6228ac4515cdd23
yashkodgirwar <yashkodgirwar@gmail.com>
2026-06-13 15:14:48 +0530

    feat(server): register /send-feedback and /get-feedbacks endpoints

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,12 +1,14 @@
 import express from "express";
 import { auth } from "../middlewares/auth.js";
-import { getPublishedCreation, getUserCreation, togglelikeCration, deleteCreation } from "../controller/userController.js";
+import { getPublishedCreation, getUserCreation, togglelikeCration, deleteCreation, sendFeedback, getFeedbacks } from "../controller/userController.js";
 
 const userRouter = express. Router();
 userRouter.get('/get-user-creations', auth, getUserCreation)
 userRouter.get('/get-published-creations', auth, getPublishedCreation)
 userRouter.post('/toggle-like-creation', auth, togglelikeCration)
 userRouter.post('/delete-creation', auth, deleteCreation)
+userRouter.post('/send-feedback', auth, sendFeedback)
+userRouter.get('/get-feedbacks', getFeedbacks)
 
 export default userRouter;
 
