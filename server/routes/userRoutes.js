e75b5b9232e23497acb788d68dbebe694cf9b0b8
yashkodgirwar <yashkodgirwar@gmail.com>
2026-06-13 14:49:17 +0530

    feat(server): update user routes configuration

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,11 +1,11 @@
 import express from "express";
 import { auth } from "../middlewares/auth.js";
-import { getPublishedCreation, getUserCreation, togglelikeCration } from "../controller/userController";
+import { getPublishedCreation, getUserCreation, togglelikeCration } from "../controller/userController.js";
 
 const userRouter = express. Router();
-userRouter. get('/get-user-creations', auth, getUserCreation)
-userRouter. get('/get-published-crations', auth, getPublishedCreation)
-userRouter. post('/toggle-like-creations', auth, togglelikeCration)
+userRouter.get('/get-user-creations', auth, getUserCreation)
+userRouter.get('/get-published-creations', auth, getPublishedCreation)
+userRouter.post('/toggle-like-creation', auth, togglelikeCration)
 
 export default userRouter;
 
