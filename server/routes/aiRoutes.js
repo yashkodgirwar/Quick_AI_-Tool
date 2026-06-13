2dfdb006e666f9651c4e28495c46b87967e1e8f8
yashkodgirwar <yashkodgirwar@gmail.com>
2026-06-13 14:49:15 +0530

    feat(server): configure endpoints for AI generation tools

--- server/routes/aiRoutes.js
+++ server/routes/aiRoutes.js
@@ -1,16 +1,16 @@
 import express from "express";
 import { auth } from "../middlewares/auth.js";
-import { generateArticle, generateBlogtitle, generateImage, removeImageObject, resumeReview } from "../controller/aiController.js";
+import { generateArticle, generateBlogtitle, generateImage, removeImageBackground, removeImageObject, resumeReview } from "../controller/aiController.js";
+import { upload } from "../configs/multer.js";
 
 const aiRouter = express.Router();
 
 aiRouter.post('/generate-article', auth, generateArticle)
 aiRouter.post('/generate-blog-title', auth, generateBlogtitle)
 aiRouter.post('/generate-image', auth, generateImage)
-aiRouter.post('/remove-image-background',upload.single('image'), auth, removeImageBackground)
-aiRouter.post('/remove-image-object', upload.single('image'),auth, removeImageObject)
-aiRouter.post('/remove-image-object',upload.single('image'), auth, removeImageObject)
-aiRouter.post('/review-resume',upload.single('resume'), auth, resumeReview)
+aiRouter.post('/remove-image-background', auth, upload.single('image'), removeImageBackground)
+aiRouter.post('/remove-image-object', auth, upload.single('image'), removeImageObject)
+aiRouter.post('/review-resume', auth, upload.single('resume'), resumeReview)
 
 
 
