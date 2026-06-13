9e951c0c114f60923c889a99d135fefa874c8b9d
yashkodgirwar <yashkodgirwar@gmail.com>
2026-06-13 15:24:07 +0530

    add backend url

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,9 @@ import './index.css'
 import App from './App.jsx'
 import {BrowserRouter} from 'react-router-dom'
 import { ClerkProvider } from '@clerk/react'
+import axios from 'axios'
+
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
